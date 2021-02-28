c27739e3b546d36f2269dda2e2a18eab7c585d56
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2021-02-28 16:41:22 +0200

    InitialCreate

---
+++ ChainOfResponsibilityPattern/Approver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibilityPattern
+{
+    /// <summary>
+    /// The 'Handler' abstract class
+    /// </summary>
+
+    public abstract class Approver
+    {
+        protected Approver successor;
+
+        public void SetSuccessor(Approver successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+}
